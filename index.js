2d9db0f325c3277c19d61be14b2c54d16d2b00a0
pratapkumarkshirsagar <51505025+pratapkumarkshirsagar@users.noreply.github.com>
2019-07-08 17:56:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 //importing node framework
-var express = require(‘express’);
+var express = require('express');
  
 var app = express();
 //Respond with "hello world" for requests that hit our root "/"
-app.get(‘/’, function (req, res) {
+app.get('/', function (req, res) {
  res.send(‘hello world’);
 });
 //listen to port 3000 by default
